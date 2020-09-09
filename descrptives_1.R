1e5cd7f59499791ebd16932520961e9585e6118f
Christopher Wilson <51476876+christopherjwilson@users.noreply.github.com>
2020-09-09 09:24:31 +0100

    added outline of all sessions

---
+++ descrptives_1.R
@@ -0,0 +1,14 @@
+#  Exploratory and descriptives example 1
+
+## Check that data are as expected
+
+### There should be x variables 
+
+## Check data they are valid
+## Check that there are no missing data
+## Deal with data quality problems
+## Check the distribution of the data
+## Check outliers
+## Summarise the data - central tendency
+
+## Summarise the data - variance
